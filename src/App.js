c6a499c7f36295ab53b5470802582dcec5f70aba
Wilson Chen <36680838+KeroseneTrampolines@users.noreply.github.com>
2023-12-07 17:50:05 -0800

    Setup gh-pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Navbar from "./components/Navbar";
 import Jumbotron from "./components/Jumbotron";
 
 function App() {
-  // it'd be cool to use a <Typewriter> to introduce wilsonistyping
   return (
     <div className="App">
       <Navbar />
